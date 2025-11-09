9ce663e7fdb72dfcc2defc81d9b00a756089236c
WyvRacersDev <142223454+WyvRacersDev@users.noreply.github.com>
2025-11-09 18:54:33 +0500

    ai tools sumn sumn idk

--- angular+socket/chat-frontend/src/app/models/Gmail.model.ts
+++ angular+socket/chat-frontend/src/app/models/Gmail.model.ts
@@ -2,5 +2,20 @@
 
 export class Gmail
 {
+    authenticated_users:string[]=[]
     
+    authenticate():Boolean
+    {
+        return true //bilal pls fill
+    }
+
+    read_from_inbox()
+    {
+        //bilal pls fill
+    }
+
+    send_email(content:string,receiver:string)
+    {
+        //bilal pls fill
+    }
 }
